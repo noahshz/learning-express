8f828c9f47f5c0087372141ed21098cf7d694b95
Noah <n.scholziii@gmail.com>
2022-08-22 14:36:23 +0200

    Update app.js

---
+++ src/node-basics/app.js
@@ -0,0 +1,8 @@
+// node package manager
+// npm --version
+
+// local dependency - use it only in current project
+// npm install <packageName>
+
+// global dependency - use it in any project
+// npm install -g <packageName>
