a0e7d36e59407267d995a0733b3029ba0a6cc751
Noah <n.scholziii@gmail.com>
2022-08-22 09:19:26 +0200

    1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # learning-express
  
+## installation
+
---
+++ src/app.js
@@ -0,0 +1,9 @@
+const amount = 12
+
+if(amount < 10) {
+    console.log("less then 10");
+} else {
+    console.log("greater then 10");
+}
+
+console.log(`template string`);
