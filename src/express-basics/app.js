a08b411245b53f095f22d5ac9852d99a83d9ec16
Noah <n.scholziii@gmail.com>
2022-08-23 13:30:33 +0200

    express tutorial

--- src/express-basics/app.js
+++ src/express-basics/app.js
@@ -1,5 +1,36 @@
 const http = require('http');
+const { readFileSync } = require('fs');
 
-const server = http.createServer((req, res) => {
-    
+// get all files
+const homePage = readFileSync('./home.html');
+
+const server = http.createServer((request, response) => {
+    // console.log(request.method);
+    // console.log(request.url);
+    response.writeHead(200, {'content-type':'text/html'});
+
+    const url = request.url;
+    switch(url) {
+        case "/":
+            // home
+            response.write(homePage);
+            break;
+        case "/about":
+            // about
+            response.write("about me");
+            break;
+        case "/contact":
+            // contact
+            response.write("contact me");
+            break;
+        default:
+            // not found
+            response.writeHead(404, {'content-type':'application/json'});
+            response.write("not found 404");
+    }
+    response.end();
+});
+
+server.listen(5000, () => {
+    console.log(`Server listening on 5000...`);
 });
