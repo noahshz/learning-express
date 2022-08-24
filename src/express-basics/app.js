db7487bb0738b4781fb71dbc919a989d192dbaa4
Noah <n.scholziii@gmail.com>
2022-08-24 10:40:28 +0200

    Update app.js

--- src/express-basics/app.js
+++ src/express-basics/app.js
@@ -1,3 +1,16 @@
-const port = process.env.PORT || 5000;
+const express = require('express');
+const app = express();
 
-console.log(process.env);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}...`);
+});
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
