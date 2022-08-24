ee0c4ad269d0730c56051d7f6251084d1773437d
Noah <n.scholziii@gmail.com>
2022-08-24 11:19:34 +0200

    public folder

--- src/express-basics/app.js
+++ src/express-basics/app.js
@@ -5,13 +5,14 @@ const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
-// express define, which ressources will be used -> folder path
+// express define, which ressources will be used -> folder path | setup static and middleware
 app.use(express.static('./public'));
 
-app.get('/',(req,res) => {
-    res.sendFile(path.resolve(__dirname, './samplesite/index.html'));
-});
+// app.get('/',(req,res) => {
+//     res.sendFile(path.resolve(__dirname, './samplesite/index.html'));
+// });
 
+// all request types
 app.all('*',(req,res) => {
     res.status(404).send('404 not found');
 });
