2acac348bbe6ab2bf45efee0ac9776d1dd65adea
Noah <n.scholziii@gmail.com>
2022-08-25 14:44:57 +0200

    began with middleware

--- src/express-basics/app.js
+++ src/express-basics/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 
-app.get("/", (req,res) => {
+// request => middleware => response
+// imports middleware function
+const logger = require('./middleware/logger.middleware');
+
+// middleware function will be passed before default callback function
+app.get("/", logger, (req,res) => {
     res.json({message: "welcome"});
-})
+});
+
+app.get("/about", logger, (req,res) => {
+    res.json({message: "all about me"});
+});
+
 
 app.listen(5000,()=>{
     console.log(`Server is listening on port 5000...`);
