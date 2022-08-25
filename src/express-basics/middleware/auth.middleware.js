81e6e71cfd2b6487d92fcce1fb0a688be109e17a
Noah <n.scholziii@gmail.com>
2022-08-25 15:12:26 +0200

    middleware

--- src/express-basics/middleware/auth.middleware.js
+++ src/express-basics/middleware/auth.middleware.js
@@ -1,5 +1,10 @@
 const auth = (req, res, next) => {
-    console.log('authorize');
-    next();
+    const {user} = req.query;
+    if(user === 'noah') {
+        req.user = {name:'noah',id:1};
+        next();
+    } else {
+        res.status(401).send('unautherize');
+    }
 }
 module.exports = auth;
