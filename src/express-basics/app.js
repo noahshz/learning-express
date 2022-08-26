22efc8d6afb6b0f4e91158c7c4cd944e51394f17
Noah <n.scholziii@gmail.com>
2022-08-26 11:27:02 +0200

    added example route

--- src/express-basics/app.js
+++ src/express-basics/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const example_router = require('./routes/example.route');
 const user_router = require('./routes/user.route');
 
 app.use(express.urlencoded({extended: false}));
@@ -7,7 +8,7 @@ app.use(express.json());
 
 // require router for base path
 app.use('/api/users', user_router);
-
+app.use('/example', example_router);
 
 app.listen(5000,()=>{
     console.log(`Server is listening on port 5000...`);
---
+++ src/express-basics/routes/example.route.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req,res) => {
+    res.status(200).json({status: "fine", message: "This is a sample message"});
+});
+
+module.exports = router;
